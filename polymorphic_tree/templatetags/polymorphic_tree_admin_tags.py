ef4f64694c4d56e1605b9d7ad50b23d9e6f3cf09
grivial <griviala@gmail.com>
2022-01-31 17:59:14 +0300

    Admin pagination bug fixed

--- polymorphic_tree/templatetags/polymorphic_tree_admin_tags.py
+++ polymorphic_tree/templatetags/polymorphic_tree_admin_tags.py
@@ -5,6 +5,7 @@ from django.utils.safestring import mark_safe
 from future.builtins import next
 from mptt.templatetags.mptt_tags import cache_tree_children
 from polymorphic_tree.templatetags.stylable_admin_list import stylable_column_repr
+from django.utils.translation import gettext as _
 
 register = Library()
 
@@ -72,7 +73,7 @@ class AdminListRecurseTreeNode(Node):
     def render(self, context):
         cl = self.cl_var.resolve(context)
         assert isinstance(cl, ChangeList), "cl variable should be an admin ChangeList"  # Also assists PyCharm
-        roots = cache_tree_children(cl.result_list)
+        roots = get_tree(cl.result_list)
         bits = [self._render_node(context, cl, node) for node in roots]
         return ''.join(bits)
 
@@ -82,6 +83,76 @@ class AdminListRecurseTreeNode(Node):
             html, row_class_ = stylable_column_repr(cl, node, field_name)
             columns.append((field_name, html))
         return columns
+
+
+def add_node(obj, current_path, top_nodes, parent_attr, root_level, is_filtered, queryset):
+    # Get the current mptt node level
+    node_level = obj.get_level()
+
+    parent = obj.parent
+
+    if parent is not None and len(top_nodes) == 0:
+        root_level = parent.get_level()
+        add_node(parent, current_path, top_nodes, parent_attr, root_level, is_filtered, queryset)
+
+    if node_level < root_level:
+        # ``queryset`` was a list or other iterable (unable to order),
+        # and was provided in an order other than depth-first
+        raise ValueError(
+            _("Node %s not in depth-first order") % (type(queryset),)
+        )
+
+    # Set up the attribute on the node that will store cached children,
+    # which is used by ``MPTTModel.get_children``
+    obj._cached_children = []
+
+    # Remove nodes not in the current branch
+    while len(current_path) > node_level - root_level:
+        current_path.pop(-1)
+
+    if node_level == root_level:
+        # Add the root to the list of top nodes, which will be returned
+        top_nodes.append(obj)
+    else:
+        # Cache the parent on the current node, and attach the current
+        # node to the parent's list of children
+        _parent = current_path[-1]
+        setattr(obj, parent_attr, _parent)
+        _parent._cached_children.append(obj)
+
+        if root_level == 0:
+            # get_ancestors() can use .parent.parent.parent...
+            setattr(obj, "_mptt_use_cached_ancestors", True)
+
+    # Add the current node to end of the current path - the last node
+    # in the current path is the parent for the next iteration, unless
+    # the next iteration is higher up the tree (a new branch), in which
+    # case the paths below it (e.g., this one) will be removed from the
+    # current path during the next iteration
+    current_path.append(obj)
+
+
+def get_tree(queryset):
+
+    current_path = []
+    top_nodes = []
+
+    if queryset:
+        # Get the model's parent-attribute name
+        parent_attr = queryset[0]._mptt_meta.parent_attr
+        root_level = None
+        is_filtered = hasattr(queryset, "query") and queryset.query.has_filters()
+        for obj in queryset:
+            node_level = obj.get_level()
+            if node_level == 0:
+                current_path.clear()
+            if len(current_path) == 0:
+                root_level = node_level
+            else:
+                root_level = current_path[0].get_level()
+            add_node(obj, current_path, top_nodes, parent_attr, root_level, is_filtered, queryset)
+
+    return top_nodes
 
 
 @register.tag
