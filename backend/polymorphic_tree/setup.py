20f097abe64d6085e993f1d6098d5fe42d07a3c0
crusat <crusat@yandex.ru>
2022-02-18 18:17:08 +0300

    2.1.1

--- backend/polymorphic_tree/setup.py
+++ backend/polymorphic_tree/setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='django-polymorphic-tree-for-garpix-page',
-    version='2.1.0',
+    version='2.1.1',
     description='',
     long_description=long_description,
     url='https://github.com/garpixcms/garpix_page',
